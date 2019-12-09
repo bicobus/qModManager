4868138e268f21e39746b4fdb924d2529b89cb05
bicobus <bicobus@keemail.me>
2019-12-09 10:25:17 +0100

    Handle CTRL+c signals

--- main.py
+++ main.py
@@ -2,13 +2,17 @@
 # Licensed under the EUPL v1.2
 # © 2019 bicobus <bicobus@keemail.me>
 import sys
+import signal
 from PyQt5.QtWidgets import QApplication
 from qmm.manager import MainWindow, logger
+
 
 if __name__ == "__main__":
     import locale
     # Sets locale according to $LANG variable instead of C locale
     locale.setlocale(locale.LC_ALL, '')
+    # Ends the application on CTRL+c
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
 
     logger.info("Starting application")
     try:
