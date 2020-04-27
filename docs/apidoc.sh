4b264b397a6decd5384028101c089a5adf2c04e4
bicobus <bicobus@keemail.me>
2020-04-28 00:25:17 +0200

    add sphinx config

---
+++ docs/apidoc.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+ARGS='--tocfile api -e -M -a'
+OUTPUT='.'
+INPUT='../qmm'
+EXCLUDE='../qmm/ui_* ../qmm/*_rc.py'
+
+SPHINX_APIDOC_OPTIONS=members,show-inheritance sphinx-apidoc $ARGS -o $OUTPUT $INPUT $EXCLUDE
