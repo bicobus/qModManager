5f70f96783361808fd24d994aceedf534cd0c30f
bicobus <bicobus@keemail.me>
2020-04-28 00:36:45 +0200

    Add a version.py file

---
+++ qmm/version.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# © 2020 bicobus <bicobus@keemail.me>
+
+VERSION_MAJOR = 1
+VERSION_MINOR = 0
+VERSION_BUILD = 0
+# Either alpha, beta, RC or stable
+VERSION_STAGE = 'alpha9'
+VERSION_INFO = (VERSION_MAJOR, VERSION_MINOR, VERSION_BUILD, VERSION_STAGE)
+
+VERSION_STRING = "%d.%d.%d-%s" % VERSION_INFO
