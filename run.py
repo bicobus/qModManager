31ac5ce6be97b2e15e313503f00bf647e73370b6
bicobus <bicobus@keemail.me>
2020-05-07 09:09:33 +0200

    Initialize gettext before anything else

--- run.py
+++ run.py
@@ -2,7 +2,11 @@
 # Licensed under the EUPL v1.2
 # © 2020 bicobus <bicobus@keemail.me>
 
-from qmm import manager
+from qmm.lang import set_gettext
+# set_gettext() install's gettext _ in the builtins
+# doing this before anything has a chance to be called.
+set_gettext()
 
 if __name__ == "__main__":
+    from qmm import manager
     manager.main()
